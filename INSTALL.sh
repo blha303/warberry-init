7591e17e503f25ba59ee68801fcd7a8387c04ec8
blha303 <stevensmith.ome@gmail.com>
2016-05-29 11:36:05 +0800

    Initial

---
+++ INSTALL.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install nbtscan python-scapy tcpdump nmap ppp sg3-utils netdiscover macchanger
+sudo pip2 install -r requirements.txt
+read -p "Continue with extra tools? " -n 1 -r
+if [[ $REPLY =~ ^[Yy] ]]; then
+  sudo apt-get install onesixtyone nikto hydra john w3af-console ettercap-text-only cryptcat ike-scan
+fi
